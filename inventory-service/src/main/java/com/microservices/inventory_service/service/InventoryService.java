fcde3591f6f5750201ec389486396a1962f2e7e2
Girish-del <girishnalawade01@gmail.com>
2025-02-19 19:13:36 -0700

    Completed the Inventory Service which will check all the Items from the Inventory.

---
+++ inventory-service/src/main/java/com/microservices/inventory_service/service/InventoryService.java
@@ -0,0 +1,18 @@
+package com.microservices.inventory_service.service;
+
+import com.microservices.inventory_service.model.Inventory;
+import com.microservices.inventory_service.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    public boolean isInStock(String skuCode, Integer quantity){
+        return inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
+    }
+}
---
+++ inventory-service/src/main/resources/db/migration/V2__add_inventory.sql
@@ -0,0 +1,5 @@
+insert into t_inventory(quantity, sku_code)
+values (100, 'iphone_15'),
+        (100, 'pixel-8'),
+        (100, 'galaxy_s23'),
+        (100, 'one_plus_12');
